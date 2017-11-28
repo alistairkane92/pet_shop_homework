a9d9e8e5e5142a7fcd42b06d287a20bbc65460fd
Alistair Kane <vplelmo@hotmail.co.uk>
2017-11-28 20:56:21 +0000

    updates pet shop

--- pet_shop/pet_shop.rb
+++ pet_shop/pet_shop.rb
@@ -6,161 +6,94 @@ def total_cash(pet_shop)
     return (pet_shop)[:admin][:total_cash]
 end
 
-def add_or_remove_cash(petshop_array, cash)
+def add_or_remove_cash(petshop, cash)
     #Method will work on both add and remove tests because
     #the integer to add in the remove test is (-10)
-    petshop_array[:admin][:total_cash] = petshop_array[:admin][:total_cash] + cash
-    return petshop_array[:admin][:total_cash]
+    petshop[:admin][:total_cash] += cash
 end
 
-def add_or_remove_cash__add(petshop_array, cash)
-    add_or_remove_cash(petshop_array, cash)
+def pets_sold(petshop)
+    petshop[:admin][:pets_sold]
 end
 
-def add_or_remove_cash__remove(petshop_array, cash)
-    add_or_remove_cash(petshop_array, cash)
+def increase_pets_sold(pet_shop, sold)
+    pet_shop[:admin][:pets_sold] += sold
+
+    #     new_amount = pets_sold(petshop) + number_sold
 end
 
-def pets_sold(petshop_array)
-    return petshop_array[:admin][:pets_sold]
-end
-
-def increase_pets_sold(pet_shop_array, sold)
-    pet_shop_array[:admin][:pets_sold] = pet_shop_array[:admin][:pets_sold] + sold
-    return pet_shop_array[:admin][:pets_sold]
-
-    #     new_amount = pets_sold(petshop_array) + number_sold
-end
-
-def stock_count(petshop_array)
+def stock_count(petshop)
     #return the length of the pets array
-    return petshop_array[:pets].length()
+    return petshop[:pets].length()
 end
 
-
-def pets_by_breed(petshop_array, name_of_breed)
+def pets_by_breed(petshop, name_of_breed)
     #count is going to count the elements of an array
-    new_array = []
-    second_array = []
+    pets = []
 
     #loops through all the pets by breed and adds to array
-    for item in petshop_array[:pets]
-        new_array.push(item[:breed])
+    for pet in petshop[:pets]
+        pets << pet if pet[:breed] == name_of_breed
     end
-
-    for item in new_array
-        if item.include?(name_of_breed)
-            second_array.push(item)
-        end
-    end
-    return second_array
+    return pets
 end
 
-def all_pets_by_breed_found(petshop_array, breed)
-    pets_by_breed(petshop_array, breed)
-end
-
-def all_pets_by_breed_not_found(petshop_array, breed)
-    pets_by_breed(petshop_array, breed)
-end
-
-def find_pet_by_name(petshop_array, name_of_pet)
-    for item in petshop_array[:pets]
-        if item[:name] == name_of_pet
-            return item
-        end
+def find_pet_by_name(petshop, name_of_pet)
+    for pet in petshop[:pets]
+        return pet if pet[:name] == name_of_pet
     end
     return nil
 end
 
-def find_pet_by_name_returns_pet(petshop_array, name_of_pet)
-    find_pet_by_name(petshop_array, name_of_pet)
-end
+def remove_pet_by_name(pet_shop, name_of_pet)
 
-def find_pet_by_name_returns_nil(petshop_array, name_of_pet)
-    find_pet_by_name(petshop_array, name_of_pet)
-end
-
-def remove_pet_by_name(petshop_array, name_of_pet)
-    new_array = []
     #locate the entry of name of pet, then remove entry
-    for item in petshop_array[:pets]
-        if item[:name] == name_of_pet
-        petshop_array[:pets].delete(item)
-        end
-    end
+    pet_shop[:pets].delete(find_pet_by_name(pet_shop, name_of_pet))
 end
 
-def add_pet_to_stock(petshop_array, new_pet)
+def add_pet_to_stock(petshop, new_pet)
     #method adds item from new pet hash and adds it to pet shop
     #test then counts total stock to see if it is +1
 
     #push the hash from newpets and add it to pets array
-    return petshop_array[:pets] << new_pet
+    petshop[:pets] << new_pet
 end
 
-def customer_pet_count(customers_array)
+def customer_pet_count(customers)
     #method takes the length of the customers pet array
-    return customers_array[:pets].length
+    return customers[:pets].length
 end
 
-def add_pet_to_customer(customers_array, new_pet)
-    #adds the hash within new_pets array to the pets array of the customers_array
-    return customers_array[:pets] << new_pet
+def add_pet_to_customer(customers, new_pet)
+    #adds the hash within new_pets array to the pets array of the customers
+    return customers[:pets] << new_pet
 end
 
-def customer_can_afford_pet(customers_array, new_pet)
+def customer_can_afford_pet(customers, new_pet)
     #expects that the second customer in the array cannot afford the pet
 
     #this method needs to compare the price of new_pet with the cash of the
     #customer and then return false
 
-    if customers_array[:cash] < new_pet[:price]
-        return false
-    else
-        return true
-    end
+    return customers[:cash] >= new_pet[:price]
 end
 
-def price_of_pet(pet_name)
-    return pet_name[:price]
-end
-
-# def customer_cash(customer)
-#     return customer[:cash]
-# end
-
-def sell_pet_to_customer(petshop_array, pet, customer)
+def sell_pet_to_customer(petshop, pet, customer)
     #method sells a pet to the customer
 
     #assess first whether pet exists for sale and if
     #customer can afford to buy then proceed with sale
 
-
+    if pet != nil
+        if customer_can_afford_pet(customer, pet)
     #increase pets sold by 1
-        increase_pets_sold(petshop_array, 1)
+        increase_pets_sold(petshop, 1)
 
     # #add the price to the pet shop
-        add_or_remove_cash__add(petshop_array, price_of_pet(pet))
+        add_or_remove_cash(petshop, pet[:price])
 
     #needs to add pet to the customers array
         add_pet_to_customer(customer, pet)
-
-end
-
-
-def sell_pet_to_customer_pet_found(petshop_array, pet, customer)
-    sell_pet_to_customer(petshop_array, pet, customer)
-end
-
-def sell_pet_to_customer_pet_not_found(petshop_array, pet, customer)
-    if find_pet_by_name_returns_pet(petshop_array, pet)
-        sell_pet_to_customer(petshop_array, pet, customer)
-    end
-end
-
-def test_sell_pet_to_customer__insufficient_funds(petshop_array, pet, customer)
-    if find_pet_by_name_returns_pet(petshop_array, pet) && customer_can_afford_pet(customer, pet)
-        sell_pet_to_customer(petshop_array, pet, customer)
+        end
     end
 end
